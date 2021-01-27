5a425d285c73eb3b860b9c88e31f25bdba874679
jrmkim50 <jrmkim50@gmail.com>
2021-01-27 16:23:01 -0500

    changed dependencies

--- soil-sight-app/src/components/map/Map.js
+++ soil-sight-app/src/components/map/Map.js
@@ -16,88 +16,87 @@ const Map = () => {
     const atlantaBBox = [-84.41531, 33.66927, -84.37179, 33.85259];
 
     useEffect(() => {
-        getGardens(getMap);
-    }, [])
-
-    function getMap(tempGardens) {
-        if (gardens.length !== 0) {
-            tempGardens = gardens;
-        }
-        var map = new window.atlas.Map("map", {
-            authOptions: authOptions
-        });
-        map.setCamera({
-            bounds: atlantaBBox,
-            zoom: 40
-        });
-        var datasource = new window.atlas.source.DataSource();
-        var resultLayer = new window.atlas.layer.SymbolLayer(datasource, null, {
-            iconOptions: {
-                image: 'pin-round-darkblue',
-                anchor: 'center',
-                allowOverlap: true
-            },
-            textOptions: {
-                anchor: "top"
+        function getMap(tempGardens) {
+            if (gardens.length !== 0) {
+                tempGardens = gardens;
             }
-        });
-        var popup = new window.atlas.Popup();
-        map.events.add('ready', async function () {
-            console.log('1');
-            map.sources.add(datasource);
-            map.layers.add(resultLayer);
-            var newPositions = positions;
-            console.log('2');
-            for (var gardenName in tempGardens) {
-                var gardenData = tempGardens[gardenName];
-                console.log('3');
-                var query = gardenData["ADDRESS"];
-                try {
-                    var results = await searchURL.searchAddress(window.atlas.service.Aborter.timeout(10000), query, {
-                        limit: 1
-                    })
-                    var data = results.geojson.getFeatures();
-                    datasource.add(data);
-                    if (data["features"]) {
-                        newPositions[data["features"][0]["geometry"]["coordinates"]] = gardenName;
-                    }
-                } catch (err) {
-                    console.log(err);
-                }
-            }
-            setPositions(newPositions);
-            map.events.add('click', resultLayer, function (e) {
-                if (e.shapes && e.shapes.length > 0) {
-                    var content, coordinate;
-                    coordinate = e.shapes[0].data.geometry.coordinates;
-                    content = `<div class="azurePopup">
-                                    <div>${positions[coordinate]}</div>
-                               </div>`;
-                    if (content && coordinate) {
-                        popup.setOptions({
-                            content: content,
-                            position: coordinate
-                        });
-                        popup.open(map);
-                        setCurrentGarden(tempGardens[positions[coordinate]]);
-                        console.log(tempGardens[positions[coordinate]])
-                    }
+            var map = new window.atlas.Map("map", {
+                authOptions: authOptions
+            });
+            map.setCamera({
+                bounds: atlantaBBox,
+                zoom: 40
+            });
+            var datasource = new window.atlas.source.DataSource();
+            var resultLayer = new window.atlas.layer.SymbolLayer(datasource, null, {
+                iconOptions: {
+                    image: 'pin-round-darkblue',
+                    anchor: 'center',
+                    allowOverlap: true
+                },
+                textOptions: {
+                    anchor: "top"
                 }
             });
-        })
-    }
-
-
-    const getGardens = async (result) => {
-        try {
-            let response = await fetch("api/garden-get");
-            let json = await response.json();
-            setGardens(json);
-            result(json);
-        } catch (err) {
-            console.log(err);
+            var popup = new window.atlas.Popup();
+            map.events.add('ready', async function () {
+                console.log('1');
+                map.sources.add(datasource);
+                map.layers.add(resultLayer);
+                var newPositions = positions;
+                console.log('2');
+                for (var gardenName in tempGardens) {
+                    var gardenData = tempGardens[gardenName];
+                    console.log('3');
+                    var query = gardenData["ADDRESS"];
+                    try {
+                        var results = await searchURL.searchAddress(window.atlas.service.Aborter.timeout(10000), query, {
+                            limit: 1
+                        })
+                        var data = results.geojson.getFeatures();
+                        datasource.add(data);
+                        if (data["features"]) {
+                            newPositions[data["features"][0]["geometry"]["coordinates"]] = gardenName;
+                        }
+                    } catch (err) {
+                        console.log(err);
+                    }
+                }
+                setPositions(newPositions);
+                map.events.add('click', resultLayer, function (e) {
+                    if (e.shapes && e.shapes.length > 0) {
+                        var content, coordinate;
+                        coordinate = e.shapes[0].data.geometry.coordinates;
+                        content = `<div class="azurePopup">
+                                        <div>${positions[coordinate]}</div>
+                                   </div>`;
+                        if (content && coordinate) {
+                            popup.setOptions({
+                                content: content,
+                                position: coordinate
+                            });
+                            popup.open(map);
+                            setCurrentGarden(tempGardens[positions[coordinate]]);
+                            console.log(tempGardens[positions[coordinate]])
+                        }
+                    }
+                });
+            })
         }
-    }
+    
+        const getGardens = async (result) => {
+            try {
+                let response = await fetch("api/garden-get");
+                let json = await response.json();
+                setGardens(json);
+                result(json);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+        getGardens(getMap);
+    }, [])
 
     return (
         <div>
